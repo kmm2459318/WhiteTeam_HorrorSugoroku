3f8177536a313107ee1181d06b10a114b7569183
kmm-2459393 <kmm2459393@stu.o-hara.ac.jp>
2025-01-20 15:11:50 +0900

    マスの色や種類を追加しました。

--- HorrorSugoroku/Assets/TekiScreputo/Event.cs
+++ HorrorSugoroku/Assets/TekiScreputo/Event.cs
@@ -15,6 +15,18 @@ public class Event : MonoBehaviour
             case "Blockl":
                 Debug.Log($"{name}: ペナルティ効果発動！");
                 break;
+            case "Item":
+                Debug.Log($"{name}:アイテムを獲得！");
+                break;
+            case " Direc":
+                Debug.Log($"{name}:演出発動！");
+                break;
+            case "Debuff":
+                Debug.Log($"{name}:デバフ効果発動！");
+                break;
+            case "Battery":
+                Debug.Log($"{name}:バッテリーを獲得！");
+                break;
             default:
                 Debug.Log($"{name}: 通常マス - 効果なし。");
                 break;
@@ -25,7 +37,7 @@ public class Event : MonoBehaviour
         // タグが"Player"のオブジェクトのみ反応
         if (other.CompareTag("Player"))
         {
-            Debug.Log($"{name} にプレイヤーが到達しました（アイテムマス）");
+            Debug.Log($"{name} にプレイヤーが到達しました");
 
           
         }
