e1fc76eeec07a0275f0c6b3e631ba1d6d511960b
2459338 <kmm2459338@stu.o-hara.ac.jp>
2025-01-21 10:15:09 +0900

    a

---
+++ HorrorSugoroku/Assets/C#Script/Player/FirstPersonCamera.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    public float mouseSensitivity = 400f; // 感度を上げる
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) // 左クリックが押されている間
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+    }
+}
