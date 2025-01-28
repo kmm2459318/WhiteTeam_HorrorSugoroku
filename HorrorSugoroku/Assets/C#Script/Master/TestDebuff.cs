a58dfb075399bb91723f7085e001342048b64898
kmm2459344 <kmm2459344@stu.o-hara.ac.jp>
2025-01-28 11:38:15 +0900

    Master修正
    β用に修正

--- HorrorSugoroku/Assets/C#Script/Master/TestDebuff.cs
+++ HorrorSugoroku/Assets/C#Script/Master/TestDebuff.cs
@@ -10,13 +10,11 @@ public class TestDebuff : MonoBehaviour
     {
         Debug.Log("ID:" + DebuffSheet.DebuffSheet[n].ID);
         Debug.Log("イベント名:" + DebuffSheet.DebuffSheet[n].Name);
-        Debug.Log("何ターン止まるか:" + DebuffSheet.DebuffSheet[n].StopTurn);
-        Debug.Log("最小移動量:" + DebuffSheet.DebuffSheet[n].BackMin);
-        Debug.Log("最大移動量:" + DebuffSheet.DebuffSheet[n].BackMax);
         Debug.Log("懐中電灯の最小ゲージ減少量:" + DebuffSheet.DebuffSheet[n].DecreaseMin);
         Debug.Log("懐中電灯の最大ゲージ減少量:" + DebuffSheet.DebuffSheet[n].DecreaseMax);
-        Debug.Log("呪いを付与するかの判定:" + DebuffSheet.DebuffSheet[n].Curse);
         Debug.Log("アイテムを付与するかの判定:" + DebuffSheet.DebuffSheet[n].ItemGive);
+        Debug.Log("アイテムが使えなくなるかの判定:" + DebuffSheet.DebuffSheet[n].ItemGive);
+        Debug.Log("アイテムが使えないターン数:" + DebuffSheet.DebuffSheet[n].ItemGive);
     }
 
 }
