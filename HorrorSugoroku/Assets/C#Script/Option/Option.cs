b1f2fc6bb23eaf4a39592d116ecfa8c6539e0960
kmm2459344 <kmm2459344@stu.o-hara.ac.jp>
2025-02-13 13:09:14 +0900

    感度完成

--- HorrorSugoroku/Assets/C#Script/Option/Option.cs
+++ HorrorSugoroku/Assets/C#Script/Option/Option.cs
@@ -5,13 +5,17 @@ using UnityEngine.UI;
 public class Option : MonoBehaviour
 {
     public GameManager gameManager;
+    public CameraController cameraController;
 
     public GameObject OptionCanvas;//オプション画面
 
     public Slider VolumeSlider;//音量バー
+    public Image VolumeImg;//音量の画像
+    public Sprite VolumeSprite;//スピーカーの画像
+    public Sprite VolumeMuteSprite;//ミュート画像
     public int Volume = 50;//音量
     public Slider SensitivitySlider;//マウス感度バー
-    public int Sensitivity = 2;// 感度
+    public float Sensitivity = 250f;// 感度
     private bool Setting = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,11 +23,17 @@ public class Option : MonoBehaviour
     {
         OptionCanvas.SetActive(false);
 
-        //音量、感度を初期化
+        //音量を初期化
         VolumeSlider.value = Volume;
+        //Debug.Log("初期音量:" + VolumeSlider.value);
+
+        //カメラ感度を初期化
         SensitivitySlider.value = Sensitivity;
-        Debug.Log("音量:" + VolumeSlider.value);
-        Debug.Log("感度///:" + SensitivitySlider.value);
+        cameraController.mouseSensitivity = Sensitivity;
+        Debug.Log("初期感度///:" + SensitivitySlider.value);
+
+
+        
     }
 
     // Update is called once per frame
@@ -31,12 +41,26 @@ public class Option : MonoBehaviour
     {
         //バーの値を反映させる
         Volume = (int)VolumeSlider.value;
-        Sensitivity = (int)SensitivitySlider.value;
         VolumeSlider.value = Volume;
+        
+        //カメラ感度変更
+        Sensitivity = SensitivitySlider.value;
         SensitivitySlider.value = Sensitivity;
+        cameraController.mouseSensitivity = Sensitivity;
+
 
         Debug.Log("変更音量:" + VolumeSlider.value);
         Debug.Log("変更感度:" + SensitivitySlider.value);
+
+        //ボリュームが0なら画像変更
+        if(Volume == 0)
+        {
+            VolumeImg.sprite = VolumeMuteSprite;
+        }
+        else
+        {
+            VolumeImg.sprite = VolumeSprite;
+        }
 
     }
 
