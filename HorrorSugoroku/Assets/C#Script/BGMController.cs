43e9bb36e32e4a9685b523b99e8883e59c0e730b
kmm2459358 <kmm2459358@stu.o-hara.ac.jp>
2025-02-18 12:37:14 +0900

    BGM修正＆敵追跡後の後ろに戻るやつ修正

---
+++ HorrorSugoroku/Assets/C#Script/BGMController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class BGMController : MonoBehaviour
+{
+    public AudioClip undetectedBGM;
+    public AudioClip discoveryBGM;
+    public EnemySaikoro enemySaikoro;
+    private AudioSource audioSource; // ‰¹ºÄ¶—p‚ÌAudioSource
+    private bool undet = false;
+    private bool dis = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>(); // AudioSource‚ª‚È‚¯‚ê‚Î’Ç‰Á
+        }
+        audioSource.loop = true;
+    }
+
+    void Update()
+    {
+        if (!enemySaikoro.discovery && !undet)
+        {
+            audioSource.Stop(); // Œ»İ‚ÌBGM‚ğ’â~
+            audioSource.clip = undetectedBGM;
+            audioSource.Play(); // –¢”­Œ©‚ÌBGM‚ğÄ¶
+            undet = true;
+            dis = false;
+        }
+        else if (enemySaikoro.discovery && !dis)
+        {
+            audioSource.Stop(); // Œ»İ‚ÌBGM‚ğ’â~
+            audioSource.clip = discoveryBGM;
+            audioSource.Play(); // ”­Œ©‚ÌBGM‚ğÄ¶
+            undet = false;
+            dis = true;
+        }
+    }
+}
